e3ea45949e5b3d79b3d57782bd84c5f7d161f7ff
Corey Schroeder <codingcorey365@gmail.com>
2023-11-27 15:01:55 -0600

    updated code

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -10,28 +10,41 @@ namespace FactoryPattern
     // Factory Pattern Exercise Repo
     
     // 1.
-    // Create a console app that utilizes factory pattern by taking a user’s input of how many tires are on a vehicle and, based on their input, creates that type of
-    // vehicle.
+    // Create a console app that utilizes factory pattern
+    // by taking a user’s input of how many tires are on a vehicle
+    // and, based on their input, creates that type of vehicle.
 
     // 2.
-    // Complete this using an interface. You must have at least 2 subclasses.For instance, if I type 4 into the console, the program would create a car or truck, or if
-    // someone types 2 it would create a motorcycle.
+    // Complete this using an interface.
+    // You must have at least 2 subclasses.
+    // For instance, if I type 4 into the console,
+    // the program would create a car or truck,
+    // or if someone types 2 it would create a motorcycle.
 
-    // Feel free to use something other than vehicles if you want to be creative, but the other constraints still apply!
+    /* Feel free to use something other than vehicles
+     * if you want to be creative,
+     * but the other constraints still apply! */
     
     // 3.
-    // Create an Interface named IVehicle that has a stubbed out public void Drive method.
+    // Create an Interface named IVehicle
+    // that has a stubbed out public void Drive method.
 
     // 4. 
-    // Create 2 new public classes that will conform to IVehicle.
+    // Create 2 new public classes
+    // that will conform to IVehicle.
     // Example) Car, Motorcycle, BigRig
-    // Note: These classes must conform to IVehicle and implement the Drive() method - which will just Console.WriteLine(“Building a new Car!”)
+    //
+    // Note: These classes must conform to IVehicle
+    // and implement the Drive() method
+    // - which will just Console.WriteLine(“Building a new Car!”)
 
     // 5.
     // Now we will make our static VehicleFactory class.
 
     // 6.
-    // It will contain a static method GetVehicle(), that will return an IVehicle based on the amount of tires it’s given as a parameter
+    // It will contain a static method GetVehicle(),
+    // that will return an IVehicle based on the
+    // amount of tires it’s given as a parameter
 
     // 7. 
     // Call this functionality in the Main method.
@@ -40,7 +53,7 @@ namespace FactoryPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, How many tires are on your vehicle you desire?");
+            Console.WriteLine("Hello, How many tires are on your vehicle do you desire?");
 
             var userInput = Console.ReadLine();
         }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    // 6.
+    // It will contain a static method GetVehicle(),
+    // that will return an IVehicle based on the
+    // amount of tires it’s given as a parameter
+    public static class VehicleFactory
+    {
+         public static IVehicle GetVehicle(string userinput) 
+        {
+            //var vehicle = new Truck(userinput);
+            //var numberOfTires = int.Parse(userinput);
+            return new Motorcyle();
+        }
+    }
+}
